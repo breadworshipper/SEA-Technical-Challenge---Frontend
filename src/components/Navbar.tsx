23f62cb59266e15122feaf48b8e0d6938e638306
breadworshipper <azhrayashna@gmail.com>
2024-06-25 11:29:37 +0700

    CHORE: Added shadow for the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ import {
 
 export default function Navbar() {
   return (
-    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6 shadow-lg">
       <Sheet>
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="lg:hidden">
