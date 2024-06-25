bad395c33432f4c693fa2c9ead8f1c3157dc7dea
breadworshipper <azhrayashna@gmail.com>
2024-06-25 11:29:54 +0700

    CHORE: Added border for the bottom of the header background

--- src/components/ui/HeaderBackground.tsx
+++ src/components/ui/HeaderBackground.tsx
@@ -1,6 +1,6 @@
 export default function HeaderBackground() {
     return (
-        <div className="absolute top-0 -z-10 h-64 w-screen bg-tint5 rounded-b-[100px]">
+        <div className="absolute top-[80px] -z-10 h-64 w-screen bg-tint5 rounded-b-[100px] border-b-2 border-deepgrey">
             
         </div>
     )
