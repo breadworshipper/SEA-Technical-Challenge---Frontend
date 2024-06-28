28728f19bfd644a808a9b8636085563c15c38b65
breadworshipper <azhrayashna@gmail.com>
2024-06-28 08:46:03 +0700

    FIX: fixed header background max width

--- src/components/ui/HeaderBackground.tsx
+++ src/components/ui/HeaderBackground.tsx
@@ -1,7 +1,5 @@
 export default function HeaderBackground() {
     return (
-        <div className="absolute top-[80px] -z-10 h-64 w-screen bg-tint5 rounded-b-[100px] border-b-2 border-deepgrey">
-            
-        </div>
+        <div className="absolute top-[80px] left-1/2 transform -translate-x-1/2 -z-10 h-64 w-screen max-w-screen-xl bg-tint5 rounded-b-[100px] border-b-2 border-deepgrey"></div>
     )
 }
