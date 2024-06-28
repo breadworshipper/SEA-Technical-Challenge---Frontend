7f33ae1d45f1f03dd8601a78f659e10a8e21a997
breadworshipper <azhrayashna@gmail.com>
2024-06-28 14:51:51 +0700

    FEAT: protect /reservation and /dashboard route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,15 @@
 import { NextRequest, NextResponse } from "next/server"
-import Cookies from "js-cookie"
 
-const protectedRoutes = ["/admin", "/reservation"]
+const protectedRoutes = ["/admin", "/reservation", "/dashboard"]
 
 export default function middleware(req: any) {
-  const verified =
-    req.cookies.get("loggedIn").value === "true" &&
-    (req.cookies.get("role").value === "Customer" ||
-      req.cookies.get("role").value === "Admin")
+const verified =
+    (req.cookies.get("loggedIn")?.value === "true" &&
+        (req.cookies.get("role")?.value === "Customer" ||
+            req.cookies.get("role")?.value === "Admin"))
 
   if (!verified && protectedRoutes.includes(req.nextUrl.pathname)) {
-    const absoluteURL = new URL("/", req.nextUrl.origin)
+    const absoluteURL = new URL("/unauthorized", req.nextUrl.origin)
     return NextResponse.redirect(absoluteURL.toString())
   }
 }
